0873b14c2e2c3791b1a0f205fd5d3f6f888e9420
Victor <victor.cumer@gmail.com>
2021-02-24 08:57:57 +0100

    Added pages 404 and 500

--- src/hello.py
+++ src/hello.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from flask_fontawesome import FontAwesome
+from weather_utils import get_current_conditions
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+fa = FontAwesome(app)
 
 @app.route('/')
-@app.route('/<name>')
-def index(name = None):
-    user_agent = request.headers.get('User-Agent')
-    return render_template('index.html', user = name, agent = user_agent)
+def index():
+    weather_data = get_current_conditions()
+    return render_template('index.html', weather_data = weather_data)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000, debug = True)
---
+++ src/weather_utils.py
@@ -0,0 +1,32 @@
+import requests
+import os
+
+def get_current_conditions():
+    """return a dict with the current weather information."""
+    
+    eng_to_french = {
+        'clear sky': 'ciel clair',
+        'few clouds': 'quelques nuages',
+        'scattered clouds': 'nuages épars',
+        'broken clouds': 'nuageux avec éclaircies',
+        'shower rain': 'averses',
+        'rain': 'pluie',
+        'thunderstorm': 'orages',
+        'snow': 'neige',
+        'mist': 'brouillard'
+    }
+    lat, lon = (45.449213, 4.254920) # Coordonnées de la base nautique
+    api_key = str(os.environ['OW_API_KEY']) # '055d500307c5fe5a7acd11bf1a46bfc8'
+    unit = 'metric'
+    base_url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units={unit}'
+    
+    r = requests.get(base_url).json()    
+    
+    result = {}
+    result['weather_descr'] = eng_to_french[r['weather'][0]['description'].lower()]
+    result['weather_icon_url'] = f'http://openweathermap.org/img/wn/{r["weather"][0]["icon"]}@2x.png' 
+    result['wind_deg'] = int(r['wind']['deg']) + 87
+    result['wind_speed'] = round(float(r['wind']['speed']) * 1.94384)
+    result['temp'] = round(float(r['main']['temp']))
+    result['temp_felt'] = round(float(r['main']['feels_like']))   
+    return result
